1f12f336fcfdcc36165d1c77b9ef80cd33d2410f
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-07-31 14:48:53 +0900

    ok

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -1,22 +1,32 @@
 import { fizzbuzz } from "./fizzbuzz";
 
-it('1を入力したら1を返す', () => {
-  // 準備
+describe('数を文字列に変換する', () => {
 
-  // 実行 & 検証
-  expect(fizzbuzz(1)).toBe('1');
-});
+  describe('その他の数の時はそのまま文字列に変換する', () => {
+    it('1を入力したら1を返す', () => {
+      // 準備
 
-it('2を入力したら2を返す', () => {
-  // 準備
+      // 実行 & 検証
+      expect(fizzbuzz(1)).toBe('1');
+    });
 
-  // 実行 & 検証
-  expect(fizzbuzz(2)).toBe('2');
-});
+    it('2を入力したら2を返す', () => {
+      // 準備
 
-it('3を入力したらFizzを返す', () => {
-  // 準備
+      // 実行 & 検証
+      expect(fizzbuzz(2)).toBe('2');
+    })
 
-  // 実行 & 検証
-  expect(fizzbuzz(3)).toBe('Fizz');
-});
+  });
+
+  describe('3の倍数を入力したら数の代わりにFizzを返す', () => {
+    it('3を入力したらFizzを返す', () => {
+      // 準備
+
+      // 実行 & 検証
+      expect(fizzbuzz(3)).toBe('Fizz');
+    });
+  })
+
+})
+
